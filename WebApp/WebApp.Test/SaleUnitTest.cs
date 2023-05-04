796ef64abfba6bca37c73371c99ac5723e2cf8a1
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-05-03 20:01:16 -0500

    Init setup

---
+++ WebApp/WebApp.Test/SaleUnitTest.cs
@@ -0,0 +1,34 @@
+﻿namespace WebApp.Test;
+
+public class SaleUnitTest1
+{
+    [Fact]
+    public void Discount20()
+    {
+        Sale sale = new Sale();
+        int result = sale.GetPriceDiscount(100, 20);
+
+
+        Assert.Equal(80, result);
+    }
+
+    [Fact]
+    public void Discount80()
+    {
+        Sale sale = new Sale();
+        int result = sale.GetPriceDiscount(100, 80);
+
+
+        Assert.Equal(20, result);
+    }
+
+    [Fact]
+    public void Discount30()
+    {
+        Sale sale = new Sale();
+        int result = sale.GetPriceDiscount(100, 30);
+
+
+        Assert.Equal(70, result);
+    }
+}
---
+++ WebApp/WebApp/Sale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace WebApp
+{
+	public class Sale
+	{
+		public Sale()
+		{
+		}
+
+		public int GetPriceDiscount(int originalPrice, int discount)
+		{
+			return originalPrice - originalPrice * discount / 100 ;
+		}
+
+        public string greet(string name)
+        {
+			return "Hello," + name;
+        }
+
+        public int getMax(int value1, int value2 ,int value3)
+        {
+            return value1;
+        }
+    }
+}
+
