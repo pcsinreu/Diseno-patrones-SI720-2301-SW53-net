6bfc4a6dd6ca547b8329bb2374ce4a8f2507117a
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-05-04 22:01:23 -0500

    Add smell function

--- WebApp/WebApp/Sale.cs
+++ WebApp/WebApp/Sale.cs
@@ -21,6 +21,51 @@ namespace WebApp
         {
             return value1;
         }
+
+
+		//Function to get final price
+		//var1 original price
+		//var2 Discount in %
+		//var3 flag to validate if the product has discount
+		//var4 flag to validate if the product is affected to taxes
+		public double Function1(double var1, double var2, bool var3, bool var4)
+		{
+
+            //extract the discount
+            double var6 = 0;
+            if (var3 == true)
+			{
+                double var5 = var1 * var2 / 100;
+                var6 = var1 - var5;
+            }
+			else
+			{
+				var6 = var1 - 0;
+
+            }
+
+
+            //Add taxes
+            double var7 = 0;
+            if (var4 == true) // validate if the product is affected
+			{
+                var1 = var7 + var7 *0.18;//adding taxes IGV
+            }
+			else
+			{
+				var1 = var7 + 0; // no tax added
+
+            }
+
+			return var7;
+		}
+
+
+
+
+
+
+
     }
 }
 
